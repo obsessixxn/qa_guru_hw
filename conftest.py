5b4b4b25471364357bf0442cbedf3b36fbae3886
Denzel <butmanovich@yandex.ru>
2025-09-05 19:26:46 +0300

    changing conftest

--- conftest.py
+++ conftest.py
@@ -3,14 +3,14 @@ from selene import browser
 from selenium import webdriver
 
 
-@pytest.fixture
-def setup_browser(scope="session"):
+@pytest.fixture(scope="function")
+def setup_browser():
     options = webdriver.FirefoxOptions()
     driver = webdriver.Firefox(options=options)
     browser.config.driver = driver
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def setup_resolution(setup_browser):
     browser.config.window_width = 1920
     browser.config.window_height = 1080
