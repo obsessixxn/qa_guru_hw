2134a7fbae2e8881ed2914acc1635c450627c1ea
Denzel <butmanovich@yandex.ru>
2025-09-11 15:46:44 +0300

    adding test_homework

--- homework2/test_homework.py
+++ homework2/test_homework.py
@@ -10,6 +10,7 @@ def test_greeting():
     age = 25
     # TODO Сформируйте нужную строку
     output = f'Привет, {name}! Тебе {age} лет.'
+    print(output)
 
     # Проверяем результат
     assert output == "Привет, Анна! Тебе 25 лет."
@@ -41,12 +42,12 @@ def test_circle():
     r = 23
     # TODO сосчитайте площадь
     area = math.pi * (r ** 2)
-
+    print(area)
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
     length = 2 * math.pi * r
-
+    print(length)
     assert length == 144.51326206513048
 
 
